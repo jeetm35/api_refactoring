f8130d3d3f53387d726e4fe56431d427753f35d8
root <root@LAPTOP-POR90PGJ.localdomain>
2018-05-15 12:53:50 -0700

    Proposal feedback and sample maven build with junit

---
+++ Sample/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+	Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
